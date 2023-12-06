7e073a38bb2913f9183f4f920bd5cd7d58f57af1
Tao03 <70267713+Tao03@users.noreply.github.com>
2023-12-06 22:39:20 +0100

    L'Alimentatore viene eseguito
    Ho aggiunto :
    1) Alimentatore.o che è l'eseguibile
    2) Alimentatore-Module.o che è l'implementazione del Header Alimentatore.h

--- Progetto/Alimentatore.c
+++ Progetto/Alimentatore.c
@@ -1,17 +1,22 @@
 # define _GNU_SOURCE /* necessary from now on */
-# define TIMER 10
+# define TIMER 2
+# include "Headers/alimentatore.h"
 # include <signal.h>
+# include <string.h>
+# include <stdio.h>
 
 //int sigaction ( int signum , const struct sigaction * act , struct sigaction * oldact );
 int nKids;
 void handle_signal(int signum){
+    printf("CHECKPOINT: segnale arrivato");
     for(int i = 0;i<nKids;i++){
         int pid = fork();
+        printf("CHECKPOINT: processo con pid %d è stato appena generato",getpid());
         if(pid==0){
             /**
              * In questa parte del codice ci sarà l'aggiunta dei processi atomo in memoria condivisa
             */
-           execv("atomo.c","prova");
+          // execv("atomo.c","ciao");
         }
     }
 }
@@ -21,13 +26,10 @@ int main(int argc, char* argv[])
     nKids = strtol(argv[0],NULL,10);
 
     void handle_signal(int signal); /* the handler */
-    struct sigaction new, old;
-    bero(&new, sizeof(new));        /* set all bytes to zero */
+    struct sigaction new, old;     
+    memset(&new,0,sizeof(new));     /* set all bytes to zero */
     new.sa_handler = handle_signal; /* set the handler */
     sigaction(SIGALRM, &new, NULL);  /* CASE 1: set new handler */
-
-
-    while(1){
-        alarm(TIMER);
-    }
+    printf("Alarm\n");
+    alarm(TIMER);
 }
---
+++ Progetto/Headers/Alimentatore-Module.c
@@ -0,0 +1,5 @@
+#include "alimentatore.h"
+#include <stdio.h>
+void creaAtomi(int nAtomi){
+    printf("Numero atomi: %d",nAtomi);
+}
