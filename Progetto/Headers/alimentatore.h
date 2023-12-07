6d62ad7a169b7061c97e29303679d3ac98ee15df
Tao03 <70267713+Tao03@users.noreply.github.com>
2023-12-07 12:00:06 +0100

    Aggiunta commento
    Commento più dettagliato sul metodo dell'alimentatore
    CreaAtomi()

--- Progetto/Headers/alimentatore.h
+++ Progetto/Headers/alimentatore.h
@@ -1,2 +1,9 @@
 //metodo per generare figli(processi atomo)
 void creaAtomi(int nAtomi);
+/**
+     * Crea atomi e li aggiunge all'interno della memoria condivisa
+     * Metodi da usare:
+     *      1) Uso di semafori
+     *      2) Accede alla memoria condivisa
+     *      3) Inserimento dei atomi in memoria condivisa
+*/
--- Progetto/Modules/alimentatore-module.c
+++ Progetto/Modules/alimentatore-module.c
@@ -2,4 +2,5 @@
 #include <stdio.h>
 void creaAtomi(int nAtomi){
     printf("Numero atomi: %d",nAtomi);
+    
 }
