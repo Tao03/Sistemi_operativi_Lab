50474276f740d52378be1b006b93c8fcd0bd20bc
Raffaele Melluso <raffaelemelluso01@gmail.com>
2023-12-11 17:56:50 +0100

    quasi finito attivatore.c e aggiornato materiale.txt

--- Progetto/Modules/attivatore-module.c
+++ Progetto/Modules/attivatore-module.c
@@ -1,8 +1,13 @@
-#include "Progetto/Headers/attivatore.h"
+#define _GNU_SOURCE
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/types.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include "../Headers/risorse.h"
+#include "../Headers/attivatore.h"
 void scegliAtomoVittima()
 {
     key_t key= 1234; //chiave del semaforo
@@ -14,26 +19,26 @@ void scegliAtomoVittima()
     semop ( id , & my_op , 1) ;//eseguo le operazioni
     if(semctl(id, 0, GETVAL, 0)==1) //se il semaforo è sincronizzato
     {
-        my_op . sem_num = 1;//scelgo il semaforo prioritario
-        my_op . sem_flg = 0;
-        my_op . sem_op = -1;//occupo il semaforo
-        semop ( id , & my_op , 1) ;//eseguo le operazioni
         if (semctl(id, 1, GETVAL, 0)==1) //se il semaforo prioritario è libero
         {
-            
-            /**
-             * 
-             * 
-             * 
-             * SEZIONE CRITICA, 
-             * 
-             * 
-             * 
-             * 
-            */
+            my_op . sem_num = 1;//scelgo il semaforo prioritario
+            my_op . sem_flg = 0;
+            my_op . sem_op = -1;//occupo il semaforo
+            semop ( id , & my_op , 1) ;//eseguo le operazioni
+
+            //sezione critica inizio
+            struct memCond *p;
+            int id=shmget(KEY_MEMORIA_CONDIVISA, sizeof(p), 0666); //ottengo id della memoria condivisa
+            p=shmat(id, NULL, SHM_RDONLY); //ottengo il puntatore alla memoria condivisa
+            int nAtomi = p->nAtomi;
+            int *vPid = p->vPid;
+            int pidVittima = vPid[rand()%nAtomi];
+            kill(pidVittima, SIGUSR1);
+            //sezione critica fine
+
             my_op . sem_op = 1;//rilascio il semaforo
             semop ( id , & my_op , 1) ;//eseguo le operazioni
-    }
+        }
     }
     
     
